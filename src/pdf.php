fe95463a17d4ba6d3429e44cc3c91789fa7837d5
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-14 15:09:25 -0800

    add print stylesheet

--- src/pdf.php
+++ src/pdf.php
@@ -27,6 +27,7 @@ $calculated = $calculator->calculate(
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link href="css/main.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" media="print" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     </head>
     <body class="pdf">
 
