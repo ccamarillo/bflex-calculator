367f4a4a81b40ab43e74a233122f50c37b8424ce
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-14 15:58:35 -0800

    pdf test

--- src/pdf.php
+++ src/pdf.php
@@ -24,15 +24,19 @@ $calculated = $calculator->calculate(
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"> -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        <link href="css/main.css" rel="stylesheet" />
-        <link href="css/main.css" media="print" rel="stylesheet" />
-        <link rel="stylesheet" type="text/css" media="print" href="css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
+        <link href="css/pdf.css" rel="stylesheet" />
     </head>
-    <body class="pdf">
+    <body>
 
-        <?php include('results.php'); ?>
+        <div id="content">
+            <div class="hero">
+                <h2>I am the hero.</h2>
+            </div>
+        </div>
+
+        <?php // include('results.php'); ?>
 
         <!-- BOOTSTRAP -->
         <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
