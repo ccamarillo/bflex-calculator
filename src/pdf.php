2c2fb20c95f3b6f606ce1050d5cd416ee2ff1307
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-14 15:45:27 -0800

    adjust stylesheet location

--- src/pdf.php
+++ src/pdf.php
@@ -28,7 +28,7 @@ $calculated = $calculator->calculate(
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link href="css/main.css" rel="stylesheet" />
         <link href="css/main.css" media="print" rel="stylesheet" />
-        <link rel="stylesheet" type="text/css" media="print" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" media="print" href="css/bootstrap.min.css">
     </head>
     <body class="pdf">
 
