880e4ef748a631a3fb16ea4494fd224d0820c7c3
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-15 20:01:19 -0800

    odf page 2

---
+++ src/pdf-pg-2.php
@@ -0,0 +1,92 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$facility_name = (string) $_GET['facility_name'];
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>The Hidden ROI of Single-Use Bronchoscopes</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="css/pdf.css" rel="stylesheet" />
+    </head>
+    <body>
+
+        <div id="content-pg-2">
+            <h1>Single-Use. Safer Investment.</h1>
+            <h2>Using only reusable bronchoscopes can be an unnecessary drain on hospital budgets.</h2>
+            <table class="two-col">
+                <tr>
+                    <td class="left">
+                        <p class="intro">On top of reprocessing costs, repairs, annual service contracts, and procedure delays when scopes aren’t available, costly infections still occur. $28,383 per treatment per case.<sup>1</sup></p>
+                        <p class="intro-2">The misconception that single-use is too expensive only clouds the solution. Sterile single-use bronchoscopes like the Glidescope® BFlex™ can help facilities eliminate these extra costs and delays. </p>
+                        <h3>
+                            Single-Use Bronchoscopes Mean
+                            <br />
+                            Improved Accessibility and Safety
+                        </h3>
+                        <table class="points">
+                            <tr>
+                                <td class="left">
+                                    <img class="reprocessing-icon" src="./img/pdf/reprocessing-icon.png" />
+                                </td>
+                                <td class="right">
+                                    <h4>Eliminate Reprocessing Costs</h4>
+                                    <p>Single-use bronchoscopes don’t require reprocessing. Getting reusables patient-ready is extensive and time-consuming: the cost for reprocessing one endoscope ranges from $50.14 to $152.66<sup>2</sup></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="left">
+                                    <img class="maintenance-icon" src="./img/pdf/maintenance-icon.png" />
+                                </td>
+                                <td class="right">
+                                    <h4>Expensive Repairs</h4>
+                                    <p>Damage to reusable bronchoscopes is possible during procedures and repairs can be expensive. One study cites $148 for repairs per use.<sup>3</sup></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="left">
+                                    <img class="infections-icon" src="./img/pdf/infections-icon.png" />
+                                </td>
+                                <td class="right">
+                                    <h4>Minimize the Risk of Cross-Contamination</h4>
+                                    <p>Single-use bronchoscopes can protect against infection. Multiple studies show that costly infections occur even when reprocessing exceeds national guidelines. This cost of treatment for each infection due to cross-contamination and subsequent infection is $28,383.<sup>4</sup></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="left">
+                                    <img class="contracts-icon" src="./img/pdf/contracts-icon.png" />
+                                </td>
+                                <td class="right">
+                                    <h4>Avoid Service Contracts</h4>
+                                    <p>Annual contract fees per bronchoscope add up and often exclude preventable damage, which can cost nearly the same in additional fees for out-of-pocket repairs and limit availability during off-site servicing. </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td class="right">
+                        <figure>
+                            “To reduce patient exposure to contaminated bronchoscopes, institutions should consider shifting from HLD to sterilization”
+                        </figure>
+                        <figcaption>
+                            Ofstead, C.L. et al. Effectiveness of Reprocessing for Flexible Bronchoscopes and Endobronchial Ultrasound Bronchoscopes. American College of Chest Physicians. 2018.
+                        </figcaption>
+                        <img class="scope" src="./img/pdf/scope.png" />
+                    </td>
+                </tr>
+            </table>
+            <div class="footnotes">
+                <p><sup>1</sup> Terjesen, C.L. et al. Early Assessment of the Likely Cost Effectiveness of Single-Use Flexible Video Bronchoscopes. PharmacoEconomics Open (2017). 1:133-14</p>
+                <p><sup>2</sup> Ofstead, C.L. et al. A Glimpse at the True Cost of Reprocessing Endoscopes: Results of a Pilot Project. www.iahcsmm.org. 2017 </p>
+                <p><sup>3</sup> Sohrt, Anne, et al. “Cost Comparison of Single-Use Versus Reusable Bronchoscopes Used for Percutaneous Dilatational Tracheostomy.” PharmacoEconomics-open (2018): 1-7.</p>
+                <p><sup>4</sup> Terjesen, C.L. et al. Early Assessment of the Likely Cost Effectiveness of Single-Use Flexible Video Bronchoscopes. PharmacoEconomics Open (2017). 1:133-14</p>
+            </div>
+        </div>
+
+    </body>
+</html>
