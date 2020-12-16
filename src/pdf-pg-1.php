0decb885558a77a481e6bdac27bc889285210036
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-16 12:16:11 -0800

    update page 1 of PDF to allow more text without disrupting the footer

--- src/pdf-pg-1.php
+++ src/pdf-pg-1.php
@@ -20,7 +20,9 @@ $facility_name = (string) $_GET['facility_name'];
         <div id="content-pg-1">
             <img class="bflex-logo" src="img/pdf/bflex-logo.png" />
             <h1><strong>Your Cost Comparison Snapshot:</strong> Single-Use vs. Reusable Bronchoscopes</h1>
-            <h2>See how current costs for reusable bronchoscopes at <strong><?php echo $facility_name; ?></strong> compare to single-use. </h2>
+            <div class="variable-text-container">
+                <h2>See how current costs for reusable bronchoscopes at <strong><?php echo $facility_name; ?></strong> compare to single-use. </h2>
+            </div>
             <img class="verathon-logo" src="img/verathon-logo.png" />
         </div>
 
