ea3284d55c502838325884c5c5c7fa40a2a629a0
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-19 09:16:16 -0800

    change title of landing page

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>The Hidden ROI of Single-Use Bronchoscopes</title>
+        <title>Bronchoscope Cost-Comparison Calculator – Verathon Inc.</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
