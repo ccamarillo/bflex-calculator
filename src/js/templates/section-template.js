898f40aa4d87441499a3e35f8f7c08a27b70883c
Candace Camarillo <candace.fox.camarillo@gmail.com>
2020-12-19 08:48:10 -0800

    add padding above questions for small widths

--- src/js/templates/section-template.js
+++ src/js/templates/section-template.js
@@ -13,23 +13,25 @@ const SectionTemplate = `
                     <h2>{{ title }}</h2>
                     <div v-html=details></div>
                 </div>
-                <div class="col-md-6 col-sm-12 mt-xs-4 mt-sm-4 mt-md-0 mt-lg-0">
-                    <questions 
-                        v-if="id !== 4" 
-                        v-for="question in questions" 
-                        :question="question"
-                        :id="question.id" 
-                        :name="question.name" 
-                        :label="question.label" 
-                        :tooltip="question.tooltip"
-                        :field_type="question.field_type"
-                        :value="question.value"
-                        :min="question.min"
-                        :max="question.max"
-                        :placeholder="question.placeholder"
-                        :max_chars="question.max_chars"
-                    ></questions>
-                    <infections v-if="id == 4"></infections> 
+                <div class="col-md-6 col-sm-12">
+                    <div class="mt-sm-5 mt-5 mt-md-0">
+                        <questions 
+                            v-if="id !== 4" 
+                            v-for="question in questions" 
+                            :question="question"
+                            :id="question.id" 
+                            :name="question.name" 
+                            :label="question.label" 
+                            :tooltip="question.tooltip"
+                            :field_type="question.field_type"
+                            :value="question.value"
+                            :min="question.min"
+                            :max="question.max"
+                            :placeholder="question.placeholder"
+                            :max_chars="question.max_chars"
+                        ></questions>
+                        <infections v-if="id == 4"></infections> 
+                    </div>
                 </div>
             </div>
         </div>
