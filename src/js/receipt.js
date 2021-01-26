f65d46833a737a8be82e11d5f0526f2e0e104eec
Candace Camarillo <candace.fox.camarillo@gmail.com>
2021-01-26 09:05:03 -0800

    cleaning up

--- src/js/receipt.js
+++ src/js/receipt.js
@@ -5,9 +5,6 @@ $(function() {
         dataType: 'text',
         success: function (data, status, jqXHR) {
             window.location = data
-            // let formData = new FormData($('form')[0]);
-            // $('input[name="blob"]').attr('value', data)
-            // $('form').submit()
         },
         error: function (msg, textStatus, errorThrown) {
             alert('An error occurred while loading the brochure.  Close this window and try again. ' + errorThrown)
